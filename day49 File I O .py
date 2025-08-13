9bdcc4a8ddff47bbc06dd7caaf680ebf7db5987a
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-08-13 22:18:25 +0530

    updaitng file io

---
+++ day49 File I O .py
@@ -0,0 +1,14 @@
+# FILE HANDELING 
+# Opening a file 
+#f = open('day31 Sets.py','r' )
+#text = f.read()
+#print(text)
+#f.close()
+
+# if you open a file that does not exist, in write mode , the new file will be created 
+f = open('day31 Sets.py','a' )
+f.write("This will be added at the last of the file ")
+f = open('day31 Sets.py','r' )
+text = f.read()
+print(text)
+f.close()
