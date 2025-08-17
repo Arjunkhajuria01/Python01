390bcd6f075931f46f6f944f8b2d746977e979f0
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-08-17 20:59:04 +0530

    update

--- day53 Map,Filter,Reduce.py
+++ day53 Map,Filter,Reduce.py
@@ -7,7 +7,48 @@
 
 # using lambda with map 
 
+# MAP : map() → Transforms each element of an iterable (changes it somehow).
+
 marks = [10,20,30,40,70,80]
 result = list(map(lambda x : x*x*x , marks))
 print(result)
+
+# Mappint multiple iterables
+
+# doing sum of values from different lists 
+a = [1,3,5,7]
+b = [2,4,6,8]
+add = list(map(lambda x , y : x+y , a , b))
+print(add)
+
+# with built in functions 
+
+elem = ["arjun" , "rajiv" , "bhavan" , "vansh" , "arush" ]
+up = list(map(str.upper , elem))
+print(up)
+
+# data cleaning 
+fruits = ["   apple   ", " banana    ", "  cherry"]
+clean = list(map(str.strip , fruits ))
+print(clean)
+
+# Mapping with dictionary 
+
+
+
+
+
+
+#FILTER : filter() → Selects elements from an iterable (keeps some, discards others).
+
+# the function given to the filter should  always return false or true 
+
+
+
+
+
+# REDUCE FUNSTION :- reduce() applies a function cumulatively to the items of an 
+# iterable (like a list).
+
+# Syntax of REDUCE : - 
 
